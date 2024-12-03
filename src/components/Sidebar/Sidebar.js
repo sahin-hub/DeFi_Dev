9d200c8fb96ddeafea1fd4849fc012a0df1523e0
Rose Parry <rose729parry@gmail.com>
2024-12-03 10:46:02 +0100

    Change presale component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -22,7 +22,6 @@ const useStyles = makeStyles(styles);
 export default function Sidebar(props) {
   const classes = useStyles();
   let location = useLocation();
-  // verifies if routeName is the one active (in browser input)
   function activeRoute(routeName) {
     return location.pathname === routeName;
   }
@@ -32,12 +31,6 @@ export default function Sidebar(props) {
       {routes.map((prop, key) => {
         var activePro = " ";
         var listItemClasses;
-        // if (prop.path === "/upgrade-to-pro") {
-        //   activePro = classes.activePro + " ";
-        //   listItemClasses = classNames({
-        //     [" " + classes[color]]: true,
-        //   });
-        // } else {
           listItemClasses = classNames({
             [" " + classes[color]]: activeRoute(prop.layout + prop.path),
           });
